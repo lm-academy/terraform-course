f307e1d961380029e743d775a0e586c1e832466d
Lauro Fialho Müller <contact@lauromueller.com>
2024-02-25 11:44:11 +0100

    feat(05-providers): add resources

---
+++ 05-providers/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us-east"
+}
+
+resource "aws_s3_bucket" "eu_west_1" {
+  bucket = "some-random-bucket-name-aosdhfoadhfu"
+}
+
+resource "aws_s3_bucket" "us_east_1" {
+  bucket   = "some-random-bucket-name-18736481364"
+  provider = aws.us-east
+}

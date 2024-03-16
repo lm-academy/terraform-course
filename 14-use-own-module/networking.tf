eba3d531280407938eb3cbda499cebdb294bf9b6
Lauro Fialho Müller <contact@lauromueller.com>
2024-03-16 10:43:53 +0100

    feat(14-use-own-module): add files

---
+++ 14-use-own-module/networking.tf
@@ -0,0 +1,21 @@
+module "networking-tf-course" {
+  source  = "udemy-lauromueller/networking-tf-course/aws"
+  version = "0.1.1"
+  
+  vpc_config = {
+    cidr_block = "10.0.0.0/16"
+    name       = "13-local-modules"
+  }
+
+  subnet_config = {
+    subnet_1 = {
+      cidr_block = "10.0.0.0/24"
+      az         = "eu-west-1a"
+    }
+    subnet_2 = {
+      cidr_block = "10.0.1.0/24"
+      public     = true
+      az         = "eu-west-1b"
+    }
+  }
+}

c4c620f9d634a4f60756b5f20de4c5077437d1ab
Lauro Fialho Müller <contact@lauromueller.com>
2024-12-23 11:37:38 +0100

    feat(proj02): add sensitive flag to passwords output

--- proj02-iam-users/users.tf
+++ proj02-iam-users/users.tf
@@ -25,6 +25,7 @@ resource "aws_iam_user_login_profile" "users" {
 }
 
 output "passwords" {
+  sensitive = true
   value = {
     for user, user_login in aws_iam_user_login_profile.users : user => user_login.password
   }
